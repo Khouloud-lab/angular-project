ffc6aec91dd516274ee20203c7f25e97dc3ea846
Khouloud <khouloudbh07@gmail.com>
2022-02-10 21:27:45 +0100

    initial project

---
+++ src/app/entreprises/entreprises.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-entreprises',
+    templateUrl: './entreprises.component.html',
+    styleUrls: ['./entreprises.component.scss']
+})
+
+export class EntreprisesComponent implements OnInit {
+  focus: any;
+  focus1: any;
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
